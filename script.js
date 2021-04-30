4e7bd496aacccee8cce12418d1833c5f462e0568
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-04-30 13:46:09 +0300

    add function

---
+++ script.js
@@ -0,0 +1,12 @@
+var total=function totalOutput(a,b,c,d){
+    console.log("Output for shade A is " + a + " litres");
+    console.log("Output for shade B is " + b + " litres");
+    console.log("Output for shade C is " + c + " litres");
+    console.log("Output for shade D is " + d + " litres");
+  
+}
+function tto(a,b,c,d){
+    return a+b+c+d;
+}
+total(510,308,486,572)
+console.log("The total otput for all shades is "+ tto(510,308,486,572)+" litres");
